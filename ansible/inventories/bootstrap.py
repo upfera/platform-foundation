d59f35eaf1a73f93171e9e2e267d6cb4abc20eda
Tomasz Marciniak <tomasz.marciniak@gmail.com>
2026-04-10 16:49:21 +0200

    chore: initial commit

--- ansible/inventories/bootstrap.py
+++ ansible/inventories/bootstrap.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 import os
 import json
+import logging
 import sys
 
-RED = '\033[91m'
-BOLD = '\033[1m'
-END = '\033[0m'
 def get_inventory():
     try:
         hosts_env = os.environ['BOOTSTRAP_HOSTS'].strip()
     except KeyError:
-        msg = f"{RED}{BOLD}ERROR!{END} {RED}BOOTSTRAP_HOSTS environment variable is required{END}"
-        print(msg, file=sys.stderr)
+        logging.error("BOOTSTRAP_HOSTS environment variable is required!")
         sys.exit(1)
 
     hosts = [h.strip() for h in hosts_env.split(',') if h.strip()]
